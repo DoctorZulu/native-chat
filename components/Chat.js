117a7696af2143475919bc032eb580c8923ccd3d
Dan <neveloffd@gmail.com>
2021-03-24 19:02:25 -0400

    fire.js base set up. Still needs configuring on firebase console.

--- components/Chat.js
+++ components/Chat.js
@@ -1,8 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import { GiftedChat } from "react-native-gifted-chat";
 import { Text } from "react-native";
+import Fire from '../Fire'
 
-const Chat = () => {
+const Chat = (props) => {
+    Chat.navigationOptions = ({navigation}) => ({
+        title: navigation.params.name
+    });
+
+    const [messages, setMessages] = useState([]);
+
+    get user() {
+        return {
+            name: props.navigation.params.name,
+            _id: Fire.shared.uid,
+        };
+    }
+
   return <Text>Hello</Text>;
 };
 
